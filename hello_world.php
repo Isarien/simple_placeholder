1e9af47189533c0a6cdb1c5b1ec1348e3aa5616b
Isarien <flament.olivier@free.fr>
2014-09-30 13:28:29 +0200

    Update hello_world.php
    Ajout du doctype.

--- hello_world.php
+++ hello_world.php
@@ -1,1 +1,13 @@
-<?php echo 'Hello World' ?>
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+  <?php echo 'Hello World' ?>
+</body>
+</html>
+
